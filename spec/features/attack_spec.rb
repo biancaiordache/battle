482d754f1b4b5b20f84a62369cb74ee6920fb5b1
Bianca <biannca.iordache@gmail.com>
2021-01-08 15:50:23 +0000

    make skinny controllers

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,15 @@
 feature 'Attacking' do
   scenario 'attack Player 2' do
     sing_in_and_play
-    click_link 'Attack'
+    click_button 'Attack'
     expect(page).to have_content 'Bianca attacked Makers'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sing_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Makers: 60HP'
+    expect(page).to have_content 'Makers: 50HP'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 describe Player do
   subject(:bianca) { Player.new('Bianca') }
+  subject(:makers) { Player.new('Makers') }
 
   describe '#name' do
     it 'returns the name' do
       expect(bianca.name).to eq 'Bianca'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(bianca.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(makers).to receive(:receive_damage)
+      bianca.attack(makers)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { bianca.receive_damage }.to change { bianca.hit_points }.by(-10)
     end
   end
 end
